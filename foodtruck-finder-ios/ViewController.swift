0b1830b66b84cf8bf40dc9cba453781a48114ab4
Jorge Andrade <jorandradefig@gmail.com>
2015-12-01 23:55:12 -0600

    Initial Commit

---
+++ foodtruck-finder-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  foodtruck-finder-ios
+//
+//  Created by Jorge Andrade on 12/1/15.
+//  Copyright © 2015 Foodtruckers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
